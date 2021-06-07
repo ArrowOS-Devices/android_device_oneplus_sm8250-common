94475313c75b22ced4e706e4bb36479f6d80d040
HELLBOY017 <abhaygill017@gmail.com>
2021-06-07 07:03:38 +0200

    sm8250-common: Add OnePlus Dolby
    Squashed commits:
    
    commit dd6e85500e3ba475a440bd79022b1d972d62569a
    Author: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Date:   Thu, 4 Nov 2021 12:23:38 +0800
    
        sm8250-common: Patch OPSoundTuner
    
        * Remove unsupported PARALLEL UserHandle.
    
    commit 537c98d374a262122b35d24e6ddc93876699bda1
    Author: HELLBOY017 <abhaygill017@gmail.com>
    Date:   Fri, 10 Sep 2021 21:29:22 +0800
    
        sm8250-common: config: Remove audio helper
    
        * This fixes unnecessary pause/play media after tuning Dolby settings.
    
    commit bbaf7fbc22568ea7f5b149333c893d2e0ef076ca
    Author: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Date:   Fri, 10 Sep 2021 21:43:55 +0800
    
        sm8250-common: prop: Add OnePlus device specific props
    
        * Some proprietary blobs may check them
    
    commit e4738613236fd53ea633ea1d91c913c707ea5177
    Author: chandu078 <chandudyavanapelli03@gmail.com>
    Date:   Fri, 10 Sep 2021 21:43:55 +0800
    
        sm8250-common: Dolby: Disable volume-leveler-enable
    
        Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    
    commit 17f0f86d474c2cbb48186927e559ff8a3fe25310
    Author: HELLBOY017 <abhaygill017@gmail.com>
    Date:   Tue, 22 Feb 2022 18:18:35 +0800
    
        sm8250-common: Update dolby
    
        [cjh1249131356]: Hide dolby in launcher]
    
    commit 199e1cb6a079b4ac0f6160de8c5ee16af438d2e5
    Author: HELLBOY017 <abhaygill017@gmail.com>
    Date:   Sat, 7 May 2022 17:47:40 +0530
    
        sm8250-common: Add dolby mediacodecs support
        - Useful for Apple Music
        - Update DaxUI to show as equalizer in some musicplayer apps
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- common.mk
+++ common.mk
@@ -359,7 +359,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
+    $(LOCAL_PATH)/configs/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
     $(LOCAL_PATH)/configs/media_codecs_kona.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_kona.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance_kona.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_kona.xml \
@@ -435,6 +435,10 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
 
 # RIL
 PRODUCT_PACKAGES += \
