6836fcaab029e387d4e03198b6b6437e4cc00366
Marc Bourgoin <themard85@gmail.com>
2021-07-04 15:02:02 -0600

    sm8250-common: Drop QCOM WFD
    Change-Id: I3dd591444dc54ebe3a32e0385cbfc3db6bfc5e1f

--- extract-files.sh
+++ extract-files.sh
@@ -60,9 +60,6 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        system_ext/lib64/libwfdnative.so)
-            sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
-            ;;
         vendor/lib64/hw/com.qti.chi.override.so)
             sed -i "s/com.oem.autotest/\x00om.oem.autotest/" "${2}"
             ;;
